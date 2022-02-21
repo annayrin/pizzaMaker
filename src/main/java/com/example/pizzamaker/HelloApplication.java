1e344a2be0c2b743f75bad49c5344db5eb3b2167
anniejoel <annhayriyan@gmail.com>
2022-02-21 17:23:12 +0400

    commiting all chnages

--- src/main/java/com/example/pizzamaker/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.pizzamaker;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
